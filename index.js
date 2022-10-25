361054d9ae2067aa6c5f2b52654f06e3edc63b7b
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2022-10-25 15:54:45 +0200

    create express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use("/public", express.static("public"));
+
+app.listen(port, (_) => {
+  console.log(`Listening on ${port} ...`);
+});
